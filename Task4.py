4848ec480977e29e0ed45cc7bfb9cb98d4cbceee
SelmarionX <113622270+SelmarionX@users.noreply.github.com>
2022-12-16 12:33:01 +0300

    Update Task4.py

--- Task4.py
+++ Task4.py
@@ -9,3 +9,16 @@ elif(quarternumber == 3):
     print('x < 0 and y < 0')
 elif(quarternumber == 4):
     print('x > 0 and y < 0')
+    
+#     number = input()
+# if (number == '1'):
+#     print("(X >0 и Y > 0)")
+# elif (number == '2'):
+#     print("(X >0 а Y < 0)")
+# elif (number == '3'):
+#     print("(X < 0 и Y < 0)")
+# elif (number == '4'):
+#     print("(X > 0 и Y < 0)")
+# else:
+#     print("Неправильный ввод")
+
